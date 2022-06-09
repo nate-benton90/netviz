eb3a9210ecad9f3891c87e80f2bb2a8590efae58
nate-benton90 <nbenton90@gmail.com>
2022-06-09 07:48:20 -0400

    rm really stupid security threat, adds tests, k8s setup

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 First things first, let's setup the environment:
-1) Run `npm install @pulumi/digitalocean`
+1) Run `npm install @pulumi/digitalocean`.
 2) If you don't have NPM, go download/configure for your machine.
-3) 
+3) Generate API token and set on machine with: `pulumi config set digitalocean:token XXXXXXXXXXXXXX --secret`.
+4) If you're really wild, go ahead with running: `pulumi up -y` (doing this will automatically preview and
+then apply your edits).
--- index.ts
+++ index.ts
@@ -1,8 +1,20 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as digitalocean from "@pulumi/digitalocean";
 
-const mainDroplet = new digitalocean.Droplet("mainFooDooVm", {
+// server for anything
+export const mainDroplet = new digitalocean.Droplet("mainFooDooVm", {
     image: "ubuntu-18-04-x64",
-    region: "nyc1",
+    region: digitalocean.Region.NYC1,
     size: "s-1vcpu-1gb",
+});
+
+// junk k8s cluster
+export const cluster = new digitalocean.KubernetesCluster("doNetViz", {
+  region: digitalocean.Region.NYC1,
+  version: "latest",
+  nodePool: {
+      name: "mainNodePool",
+      size: digitalocean.DropletSlug.Droplet1GB,
+      nodeCount: 1,
+  },
 });
