3c38ac8f35cf168cf2fa3359c40906f590a95c8c
nate-benton90 <nbenton90@gmail.com>
2022-06-09 21:06:37 -0400

    firewall added after manual one rm, still working on k8s setup bc of stupid naming thing

--- index.ts
+++ index.ts
@@ -8,13 +8,41 @@ export const mainDroplet = new digitalocean.Droplet("mainFooDooVm", {
     size: "s-1vcpu-1gb",
 });
 
-// junk k8s cluster
-export const cluster = new digitalocean.KubernetesCluster("doNetViz", {
-  region: digitalocean.Region.NYC1,
-  version: "latest",
-  nodePool: {
-      name: "mainNodePool",
-      size: digitalocean.DropletSlug.Droplet1GB,
-      nodeCount: 1,
-  },
+export const webFirewall = new digitalocean.Firewall("mainDFirewall", {
+  inboundRules: [
+      {
+          protocol: "tcp",
+          portRange: "80",
+          sourceAddresses: [
+              "0.0.0.0/0",
+          ],
+      },
+      {
+          protocol: "tcp",
+          portRange: "443",
+          sourceAddresses: [
+              "0.0.0.0/0",
+          ],
+      },
+  ],
+  outboundRules: [
+      {
+          protocol: "tcp",
+          portRange: "80",
+          destinationAddresses: [
+              "0.0.0.0/0",
+          ],
+      },
+  ],
 });
+
+// junk k8s cluster
+// export const cluster = new digitalocean.KubernetesCluster("pool-bp4h5quyy", {
+//   region: digitalocean.Region.NYC1,
+//   version: "latest",
+//   nodePool: {
+//       name: "pool-bp4h5quyy",
+//       size: digitalocean.DropletSlug.Droplet1GB,
+//       nodeCount: 1,
+//   },
+// });
