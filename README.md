b5607157bdf2ef4be5535a1ae43e76b46c4acbad
nate-benton90 <nbenton90@gmail.com>
2022-06-11 18:29:13 -0400

    rm droplet, keep k8s, docs enhancements

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-First things first, let's setup the environment:
+# How-to et al.
+
+* First things first, let's setup the environment:
 1) Run `npm install @pulumi/digitalocean`.
 2) If you don't have NPM, go download/configure for your machine.
 3) Generate API token and set on machine with: `pulumi config set digitalocean:token XXXXXXXXXXXXXX --secret`.
 4) If you're really wild, go ahead with running: `pulumi up -y` (doing this will automatically preview and
 then apply your edits).
+
+
+* As much as I sometimes dislike doing this, let's establish the (unit) testing framework:
+1) Run: `npm install mocha @types/mocha ts-node`.
+2) ...
--- index.ts
+++ index.ts
@@ -2,11 +2,11 @@ import * as pulumi from "@pulumi/pulumi";
 import * as digitalocean from "@pulumi/digitalocean";
 
 // server for anything
-export const mainDroplet = new digitalocean.Droplet("mainFooDooVm", {
-    image: "ubuntu-18-04-x64",
-    region: digitalocean.Region.NYC1,
-    size: "s-1vcpu-1gb",
-});
+// export const mainDroplet = new digitalocean.Droplet("mainFooDooVm", {
+//     image: "ubuntu-18-04-x64",
+//     region: digitalocean.Region.NYC1,
+//     size: "s-1vcpu-1gb",
+// });
 
 // default junk firewall
 export const webFirewall = new digitalocean.Firewall("mainDFirewall", {
@@ -42,7 +42,7 @@ const cluster = new digitalocean.KubernetesCluster("do-cluster", {
   region: digitalocean.Region.NYC3,
   version: digitalocean.getKubernetesVersions().then(p => p.latestVersion),
   nodePool: {
-      name: "default",
+      name: "main-pool",
       size: digitalocean.DropletSlug.DropletS1VCPU2GB,
       nodeCount: 1,
   },
