e4c51a2494dfd10a93238c2631d3126aaed03a3d
nate-benton90 <nbenton90@gmail.com>
2022-07-06 20:47:49 -0400

    asdf

--- scooby-dooby-scripts/ci-deploy.sh
+++ scooby-dooby-scripts/ci-deploy.sh
@@ -4,10 +4,6 @@ set -e
 # default sha string made from circleci container
 COMMIT_SHA1=$CIRCLE_SHA1
 export COMMIT_SHA1=$COMMIT_SHA1
-# MONGO_SYS_UN=$MONGO_SYS_UN
-# export MONGO_SYS_UN=$MONGO_SYS_UN
-# MONGO_PW=$MONGO_PW
-# export MONGO_PW=$MONGO_PW
 
 # for nginx
 envsubst < ./k8s-general/app-deployment.yml > ./k8s-general/app-deployment.yml.out
@@ -17,9 +13,9 @@ mv ./k8s-general/app-deployment.yml.out ./k8s-general/app-deployment.yml
 envsubst < ./k8s-general/app-deployment-flask.yml > ./k8s-general/app-deployment-flask.yml.out
 mv ./k8s-general/app-deployment-flask.yml.out ./k8s-general/app-deployment-flask.yml
 
-# for secrete(s) creation 
-envsubst < ./k8s-general/db-mongo-secrets.yml > ./k8s-general/db-mongo-secrets.yml.out
-mv ./k8s-general/db-mongo-secrets.yml.out ./k8s-general/db-mongo-secrets.yml
+# for secrete(s) postgres creation 
+envsubst < ./k8s-general/db-postgres-values.yml > ./k8s-general/db-postgres-values.yml.out
+mv ./k8s-general/db-postgres-values.yml.out ./k8s-general/db-postgres-values.yml
 
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
